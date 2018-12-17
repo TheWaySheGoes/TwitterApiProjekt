beb53d80bc4eb1100c491a82e424e24b45e3abea
Henrik <henrik-sandstrom@outlook.com>
2018-12-17 12:57:26 +0100

    Json test

---
+++ main.js
@@ -0,0 +1,13 @@
+﻿$( document ).ready(function() {
+	$.getJSON( "http://data.riksdagen.se/personlista/?iid=0695090885212&fnamn=&enamn=&f_ar=&kn=&parti=&valkrets=&rdlstatus=&org=&utformat=json&termlista=", function( data ) {
+		console.log(data);
+		
+		var personData = data;
+		
+		$("body").append(data["personlista"]["person"]["tilltalsnamn"] + " " + data["personlista"]["person"]["efternamn"]);
+		$("body").append("<img src='" + data["personlista"]["person"]["bild_url_80"] + "'>");
+		/*var items = [];
+		$.each( data, function( key, val ) {
+		items.push( "<li id='" + key + "'>" + val + "</li>" );*/
+	});
+});
