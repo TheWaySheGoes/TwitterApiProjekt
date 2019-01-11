8075dea01a3d49f6f20423f82e9f7317d7073c30
SandstromHenrik <henrik-sandstrom@outlook.com>
2019-01-11 14:41:19 +0100

    ManageJson.java, votes, parliament member info update

--- RiksdagServiceProject/src/main/ManageJson.java
+++ RiksdagServiceProject/src/main/ManageJson.java
@@ -1,12 +1,16 @@
 package main;
 
-import java.io.FileNotFoundException;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -17,96 +21,45 @@ import org.xml.sax.helpers.XMLReaderFactory;
 import org.xml.sax.Attributes;
 import org.xml.sax.helpers.DefaultHandler;
 
+/**
+ * Handles JSON- and XML-files from riksdagens API and removes unneeded data,
+ * divides them to smaller & more manageable sorted files.
+ * @author Henrik
+ */
 public class ManageJson {
-	
+
+	public static ManageJson jsonHandler = new ManageJson();
+	public static final int INDENT_FACTOR = 1;
+
 	public static void main(String[] args) throws MalformedURLException, SAXException, IOException {
-		ManageJson jsonHandler = new ManageJson();
-		jsonHandler.getParliamentVotes();
-		//createParliamentList();
+		//jsonHandler.getParliamentVotes();
+		//jsonHandler.createParliamentMembersList();
+		//jsonHandler.getAllVotes();
+		jsonHandler.createParliamentMemberFiles();
 	}
-	
+
+	/*
+	 * Fetches all votes from riksdagen by a given period (2018/2019).
+	 */
 	public void getParliamentVotes() throws SAXException, MalformedURLException, IOException {
 		String allVotes = "http://data.riksdagen.se/voteringlista/?rm=2018%2F19&bet=&punkt=&valkrets=&rost=&iid=&sz=500&utformat=xml&gruppering=votering_id";
-		
+
 		XMLReader myReader = XMLReaderFactory.createXMLReader();
 		myReader.setContentHandler(new RiksdagenXMLHandler());
-		myReader.parse(new InputSource(new URL(allVotes).openStream()));
-		
-		//System.out.println(myReader.getProperty("voteringlista"));
-		
-		
-		try {
-			FileWriter fw = new FileWriter("files/allVotes.xml");
-			fw.write(myReader.toString());
-			fw.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-		
-	public void createParliamentList() {
-		JSONObject jsonObj = new JSONObject(readFile("files/jumbo.json"));
-		JSONObject personlistaObj = jsonObj.getJSONObject("personlista");
-		JSONArray personArray = personlistaObj.getJSONArray("person");
-		
-		JSONArray parliamentList = new JSONArray();
-		
-		for (int i = 0; i < personArray.length(); i++) {
-			JSONObject person = personArray.getJSONObject(i);
-			JSONObject newPerson = new JSONObject();
-				
-			newPerson.append("firstName", person.get("tilltalsnamn"));
-			newPerson.append("lastName", person.get("efternamn"));
-			newPerson.append("party", person.get("parti"));
-			newPerson.append("image", person.get("bild_url_max"));
-			
-			
-			parliamentList.put(newPerson);
-		}
-		
-		try {
-			FileWriter fw = new FileWriter("files/parliamentList.json");
-			fw.write(parliamentList.toString(10));
-			fw.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
+		myReader.parse(new InputSource(new URL(allVotes).openStream()));	
 	}
 	
-	public static String readFile(String path) {
-		FileReader fr;
-		StringBuilder tempStr = new StringBuilder();
-		try {
-			fr = new FileReader(path);
-
-			int tempInt;
-
-			while ((tempInt = fr.read()) != -1) {
-
-				tempStr.append((char) tempInt);
-			}
-			// System.out.println(tempStr.toString());
-			fr.close();
-
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return tempStr.toString();
-
-	}
-	
+	/*
+	 * Converts the votes XML document to JSON format and saves it as a list to the the files folder.
+	 */
 	class RiksdagenXMLHandler extends DefaultHandler {
 		JSONArray voteList = new JSONArray();
 		JSONObject vote;
 		String parameterName = "";
-		
+
 		public void startDocument() {}
 		public void endDocument() {
-			System.out.println("yo");
-			writeFile("allVotes", voteList.toString());
+			writeFile("votes/voteList", voteList.toString(INDENT_FACTOR));
 		}
 		public void startElement(String nameSpaceURI, String localName, String qName, Attributes atts) {
 			if(qName == "votering") {
@@ -126,13 +79,119 @@ public class ManageJson {
 					parameterValue += ch[i];
 				}
 				if(!parameterValue.contains("\n")) {
-					vote.append(parameterName, parameterValue);
+					vote.put(parameterName, parameterValue);
 				}				
 			}			
 		}
 	}
+
+	/*
+	 * Creates one list with minimal information and one with more detailed information about all parliament members.
+	 */
+	public void createParliamentMembersList() throws MalformedURLException, IOException {
+		JSONObject jsonObj = new JSONObject(readFile("files/jumbo.json"));
+		JSONObject personlistaObj = jsonObj.getJSONObject("personlista");
+		JSONArray personArray = personlistaObj.getJSONArray("person");
+
+		JSONArray parliamentList = new JSONArray();
+		JSONArray parliamentDetailedList = new JSONArray();
+
+		for (int i = 0; i < personArray.length(); i++) {
+			JSONObject person = personArray.getJSONObject(i);
+			JSONObject newPerson = new JSONObject();
+
+			//Creates short list with all parliament members
+			String parliamentMemberId = (String) person.get("intressent_id");
+			newPerson.put("id",  parliamentMemberId);
+			newPerson.put("firstName", person.get("tilltalsnamn"));
+			newPerson.put("lastName", person.get("efternamn"));
+			newPerson.put("party", person.get("parti"));
+			newPerson.put("image", person.get("bild_url_max"));			
+			parliamentList.put(newPerson);
+			
+			//Creates a more detailed list
+			JSONObject newPersonFull = new JSONObject(newPerson.toString());
+			newPersonFull.put("birthYear", person.get("fodd_ar"));
+			newPersonFull.put("constituency", person.get("valkrets"));
+			newPersonFull.put("status", person.get("status"));
+			JSONArray personExtraInfo = person.getJSONObject("personuppgift").getJSONArray("uppgift");
+			for(int k = 0; k < personExtraInfo.length(); k++) { //Gets email and other extra person info.
+				if(personExtraInfo.getJSONObject(k).getString("kod").equals("Officiell e-postadress")) {
+					newPersonFull.put("email", ((String) personExtraInfo.getJSONObject(k).getJSONArray("uppgift").get(0)).replace("[på]", "@"));
+				}
+			}
+			parliamentDetailedList.put(newPersonFull);
+		}		
+		writeFile("parliamentMembers/parliamentList", parliamentList.toString(INDENT_FACTOR));
+		writeFile("parliamentMembers/parliamentDetailedList", parliamentDetailedList.toString(INDENT_FACTOR));
+	}
+
+	/*
+	 * Fetches more detailed information about all saved votes and saves them as individual files.
+	 */
+	public void getAllVotes() throws MalformedURLException, IOException {
+		JSONArray voteList = new JSONArray(readFile("files/votes/voteList.json"));
+		for (int i = 0; i < voteList.length(); i++) {			
+			String voteId = voteList.getJSONObject(i).getString("votering_id");		
+			writeFileFromURL("files/votes/full/" + voteId + ".json", "http://data.riksdagen.se/votering/" + voteId + "/json");
+
+			JSONObject voteFile = new JSONObject(readFile("files/votes/full/" + voteId + ".json"));
+			JSONObject voteInfo = voteFile.getJSONObject("votering").getJSONObject("dokument");
+
+			JSONObject voteShortInfo = new JSONObject();
+			voteShortInfo.put("docId", voteInfo.get("dok_id"));
+			voteShortInfo.put("title", voteInfo.get("titel"));
+			voteShortInfo.put("date", voteInfo.get("datum"));
+			voteShortInfo.put("pdfFile", "data.riksdagen.se/fil/" +  voteId);
+			writeFile("votes/minimal/" + voteId, voteShortInfo.toString(INDENT_FACTOR));
+		}		
+		
+	}
 	
+	/*
+	 * Creates a detailed JSON file for every parliament member.
+	 */
+	public void createParliamentMemberFiles() {
+		JSONArray memberList = new JSONArray(readFile("files/parliamentMembers/parliamentDetailedList.json"));		
+		JSONObject parliamentMembers = new JSONObject();		
+		for(int i = 0; i < memberList.length(); i++) {
+			JSONObject parliamentMember = memberList.getJSONObject(i);
+			parliamentMember.put("votes", new JSONArray());
+			parliamentMembers.put(parliamentMember.getString("id"), parliamentMember);			
+		}
+		
+		JSONArray voteList = new JSONArray(readFile("files/votes/voteList.json"));
+		for(int i = 0; i < voteList.length(); i++) {
+			JSONObject vote = voteList.getJSONObject(i);
+			vote = new JSONObject(readFile("files/votes/full/" + vote.getString("votering_id") + ".json"));
+			JSONArray votes = vote.getJSONObject("votering").getJSONObject("dokvotering").getJSONArray("votering");
+			
+			for(int k = 0; k < votes.length(); k++) {
+				vote = votes.getJSONObject(k);
+				JSONObject voteShortInfo = new JSONObject();
+				voteShortInfo.put("id", vote.get("votering_id"));
+				voteShortInfo.put("vote", vote.get("rost"));
+				voteShortInfo.put("date", vote.get("datum"));
+				if(parliamentMembers.has(vote.getString("intressent_id"))) { //If not off duty or similar
+					parliamentMembers.getJSONObject(vote.getString("intressent_id")).getJSONArray("votes").put(voteShortInfo);	
+				}							
+			}
+		}		
+		
+		for(int i = 0; i < memberList.length(); i++) {
+			String id = memberList.getJSONObject(i).getString("id");
+			JSONObject parliamentMember = parliamentMembers.getJSONObject(id);
+			writeFile("parliamentMembers/minimal/" + parliamentMember.getString("firstName") + "_" + parliamentMember.getString("lastName"), parliamentMember.toString(INDENT_FACTOR));
+		}
+	}
+
+	public void writeFileFromURL(String fileName, String URL) throws MalformedURLException, IOException {
+		InputStream in = new URL(URL).openStream();
+		Files.copy(in, Paths.get(fileName), StandardCopyOption.REPLACE_EXISTING);
+	}
+
 	public void writeFile(String fileName, String data) {
+		System.out.println(fileName);
 		try {
 			FileWriter fw = new FileWriter("files/" + fileName + ".json");
 			fw.write(data);
@@ -140,6 +199,30 @@ public class ManageJson {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}	
+	
+	public static String readFile(String path) {
+		FileReader fr;
+		StringBuilder tempStr = new StringBuilder();
+		try {
+			fr = new FileReader(path);
+
+			int tempInt;
+
+			while ((tempInt = fr.read()) != -1) {
+
+				tempStr.append((char) tempInt);
+			}
+			fr.close();
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return tempStr.toString();
+
 	}
+
 
 }
